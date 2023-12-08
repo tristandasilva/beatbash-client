d1bc223312dd458795443f751f44d21748efa196
Tristan Dasilva <67392244+tristandasilva@users.noreply.github.com>
2023-12-07 23:01:33 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
